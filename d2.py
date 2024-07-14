f0d8311aca563c007c22087bb7c6abd03e2b09b5
CyberBaks <just.alex455@gmail.com>
2024-07-14 04:20:23 +0300

    Update d2.py
    update

--- d2.py
+++ d2.py
@@ -1,29 +1,34 @@
-with open('recipes.txt', encoding='utf-8') as file:
-    cook_book = {}
-    for e in file:
-        recipes_name = e.strip()
-        ingredients_count = file.readline()
-        ingredients = []
-        for t in range(int(ingredients_count)):
-            recipes = file.readline().strip().split(' | ')
-            product, quantity, measure = recipes
-            ingredients.append({'ingredient_name': product, 'quantity': int(quantity), 'measure': measure})
-        file.readline()
-        cook_book[recipes_name] = ingredients
+def new_cook_book(file_path):
+	with open(file_path, encoding='utf-8') as file:
+		cook_book = {}
+		for e in file:
+			recipes_name = e.strip()
+			ingredients_count = file.readline()
+			ingredients = []
+			for t in range(int(ingredients_count)):
+				recipes = file.readline().strip().split(' | ')
+				product, quantity, measure = recipes
+				ingredients.append({'ingredient_name': product, 'quantity': int(quantity), 'measure': measure})
+			file.readline()
+			cook_book[recipes_name] = ingredients
+	return cook_book
 
 
-def get_shop_list_by_dishes(dishes, person_count):
-    result_list = {}
-    for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                ingredient_1 = ingredient['ingredient_name']
-                quantity = ingredient['quantity'] * person_count
-                if ingredient_1 in result_list:
-                    result_list[ingredient_1] += quantity
-                else:
-                    result_list[ingredient_1] = {'measure': measure, 'quantity': quantity}
-        return result_list
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
+	result_list = {}
+	for dish in dishes:
+		if dish in cook_book:
+			for ingredient in cook_book[dish]:
+				ingredient_name = ingredient['ingredient_name']
+				quantity = ingredient['quantity'] * person_count
+				if ingredient_name in result_list:
+					result_list[ingredient_name]['quantity'] += quantity
+				else:
+					result_list[ingredient_name] = {'measure': ingredient['measure'], 'quantity': quantity}
+	return result_list
 
-new_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+file_path = 'recipes.txt'
+cook_book = new_cook_book(file_path)
+new_list = get_shop_list_by_dishes(cook_book, ['Омлет', 'Омлет'], 2)
 print(new_list)
